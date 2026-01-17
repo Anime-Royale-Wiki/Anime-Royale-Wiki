e8ed80f4886e2610344f472eb1a9711d14d45847
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-01-17 21:29:04 +0545

    disabled tailwind preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./ar_wiki/docs/**/*.md", "./ar_wiki/docs/Templates/**/*.html"],
-    theme: { extend: {} },
-    plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    "./ar_wiki/docs/**/*.md", 
+    "./ar_wiki/docs/Templates/**/*.html",
+    "./ar_wiki/docs/**/*.js" 
+  ],
+  theme: { 
+    extend: {} 
+  },
+  plugins: [],
 }
