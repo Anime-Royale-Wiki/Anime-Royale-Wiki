75815f1564f89423ac3c4a20ee4e221cb726dd80
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-01-19 21:30:00 +0545

    config based data loading

--- ar_wiki/src/Components/data_loader.py
+++ ar_wiki/src/Components/data_loader.py
@@ -1,6 +1,8 @@
 import json
+from ar_wiki.src.config import PROJECT_ROOT
 
-def load_data(file_name):
+def load_data():
+    file_name = PROJECT_ROOT / "data" / "units.json"
     with open(file_name, 'r') as file:
         data = json.load(file)
         return data
