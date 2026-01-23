7a3a42c14418a740de05dcf6676e978686b9f0e3
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-01-23 20:45:14 +0545

    load_unit_data name change

--- ar_wiki/src/Components/data_loader.py
+++ ar_wiki/src/Components/data_loader.py
@@ -1,7 +1,7 @@
 import json
 from ar_wiki.src.config import PROJECT_ROOT
 
-def load_data():
+def load_unit_data():
     file_name = PROJECT_ROOT / "data" / "cleaned_data" / "units.json"
     with open(file_name, 'r') as file:
         data = json.load(file)
